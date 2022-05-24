6ba9e61bcac3122cb7d72151466e1a3d337145e2
H-Ra252 <101177330+H-Ra252@users.noreply.github.com>
2022-05-24 20:36:17 +0800

    Create WindowsFX11.sh

---
+++ WindowsFX11.sh
@@ -0,0 +1,26 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm -rf ngrok-stable-linux-amd64.zip
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP
+nohup ./ngrok tcp --region ap 5900 &>/dev/null &
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+qemu-img create -f raw windowsfx11.img 64G
+wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
+wget -O windowsfx11.iso 'https://onboardcloud.dl.sourceforge.net/project/windowsfxdevil/windowsfx-11.1.1109-plasma-win11-theme.iso'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 10000M \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=windowsfx11.iso,media=cdrom \
+  -drive file=windowsfx11.img,format=raw,if=virtio \
+  -drive file=virtio-win.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -smp cores=8 \
